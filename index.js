6144a2c42e55826ff145ffce6801da4b5da04f70
Beata Kosmider <kosmibe@gmail.com>
2023-02-01 14:24:08 +0100

    task is done

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const modifyEverySecondElement = (arr) => {
+  const modifiedArr = arr.map((item, index) => {
+    if (index % 2 === 1) {
+      return ++item;
+    }
+    return item;
+  });
+  return modifiedArr;
   // Zaimplementuj zadanie zgodnie z wytycznymi
 };
-
 module.exports = modifyEverySecondElement;
